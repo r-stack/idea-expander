568fcff909495453f663100cbf967fe43531e775
Kyohei Morihiro <khmorihiro@gmail.com>
2019-08-06 20:48:43 +0900

    Add bulma css

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,8 @@ export default {
 </script>
 
 <style>
+@import '../node_modules/bulma/css/bulma.css';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
