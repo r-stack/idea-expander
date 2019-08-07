fa00ea33a7af1ea834e958cca7cdfbc824c140eb
Kyohei Morihiro <khmorihiro@gmail.com>
2019-08-07 12:25:44 +0900

    Change top navbar style

--- src/components/PlayRoom.vue
+++ src/components/PlayRoom.vue
@@ -1,35 +1,39 @@
 <template>
   <section id="playroom" class="columns is-multiline">
     <nav class="navbar is-fixed-top">
-      <div class="navbar-item">
-        <span class="title">テーマ: {{ user ? user.room : '' }}</span>
+      <div class="navbar-start">
+        <div class="navbar-item">
+          <span class="title">テーマ: {{ user ? user.room : '' }}</span>
+        </div>
       </div>
-      <div class="navbar-item">
-        <span class="title">カード総数: {{ cards ? Object.keys(cards).length : '' }}</span>
-      </div>
-      <div class="navbar-item">
-        <img src="./../assets/card_create3_25.png"
-         v-if="user.badges.createBadge >= 10 & user.badges.createBadge < 20" />
-        <img src="./../assets/card_create2_25.png"
-         v-else-if="user.badges.createBadge >= 20 & user.badges.createBadge < 30" />
-        <img src="./../assets/card_create1_25.png"
-         v-else-if="user.badges.createBadge >= 30" />
-      </div>
-      <div class="navbar-item">
-        <img src="./../assets/combo3_25.png"
-         v-if="user.badges.mentionBadge >= 10 & user.badges.mentionBadge < 20" />
-        <img src="./../assets/combo2_25.png"
-         v-else-if="user.badges.mentionBadge >= 20 & user.badges.mentionBadge < 30" />
-        <img src="./../assets/combo1_25.png"
-         v-else-if="user.badges.mentionBadge >= 30" />
-      </div>
-      <div class="navbar-item">
-        <img src="./../assets/favorite3_25.png"
-         v-if="user.badges.favBadge >= 10 & user.badges.favBadge < 20" />
-        <img src="./../assets/favorite2_25.png"
-         v-else-if="user.badges.favBadge >= 20 & user.badges.favBadge < 30" />
-        <img src="./../assets/favorite1_25.png"
-         v-else-if="user.badges.favBadge >= 30" />
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <span class="title">カード総数: {{ cards ? Object.keys(cards).length : '' }}</span>
+        </div>
+        <div class="navbar-item">
+          <img src="./../assets/card_create3_25.png"
+          v-if="user.badges.createBadge >= 10 & user.badges.createBadge < 20" />
+          <img src="./../assets/card_create2_25.png"
+          v-else-if="user.badges.createBadge >= 20 & user.badges.createBadge < 30" />
+          <img src="./../assets/card_create1_25.png"
+          v-else-if="user.badges.createBadge >= 30" />
+        </div>
+        <div class="navbar-item">
+          <img src="./../assets/combo3_25.png"
+          v-if="user.badges.mentionBadge >= 10 & user.badges.mentionBadge < 20" />
+          <img src="./../assets/combo2_25.png"
+          v-else-if="user.badges.mentionBadge >= 20 & user.badges.mentionBadge < 30" />
+          <img src="./../assets/combo1_25.png"
+          v-else-if="user.badges.mentionBadge >= 30" />
+        </div>
+        <div class="navbar-item">
+          <img src="./../assets/favorite3_25.png"
+          v-if="user.badges.favBadge >= 10 & user.badges.favBadge < 20" />
+          <img src="./../assets/favorite2_25.png"
+          v-else-if="user.badges.favBadge >= 20 & user.badges.favBadge < 30" />
+          <img src="./../assets/favorite1_25.png"
+          v-else-if="user.badges.favBadge >= 30" />
+        </div>
       </div>
     </nav>
     <div class="columns is-multiline is-mobile">
